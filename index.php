935185666e0a3fcb3842735ed1909a71814a877b
Graham <graham.burek1@gmail.com>
2015-11-18 15:05:05 -0500

    Added placeholder image, started making basic site template in index.html

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Limbo Lost & Found</title>
+	<!-- Always include this link to the shared stylesheet. To add more style for a specific page or group of pages, add new link element under shared link! -->
+	<link rel="stylesheet" type="text/css" href="templates/sharedStyle.css">
+</head>
+<body>
+<!-- Navbar include statement: -->
+<?php 
+	require('templates/navbar.php');
+?>
+<!-- Main white form for pages: -->
+<div id="mainForm">
+<form>
+	Field 1
+	<input type="text" name="text1"><br>
+	Field 2
+	<input type="text" name="text2"><br>
+	Field 3
+	<input type="text" name="text3"><br>
+	Field 4
+	<input type="text" name="text4"><br>
+</form>
+</div>
+</body>
+</html>
---
+++ templates/navbar.php
@@ -0,0 +1,8 @@
+<div id="navbar">
+<ul>
+	<li><a href="index.php">Limbo Lost & Found</a></li>
+	<li><a href="found.php">Found Something?</a></li>
+	<li><a href="lost.php">Lost Something?</a></li>
+	<li><a href="#">Recent Listings</a></li>
+</ul>
+</div>
