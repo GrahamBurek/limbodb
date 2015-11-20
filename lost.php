358b6e581a56818abcbe0c4a7e88622b247b9276
Maxim Vitkin <max.vitkin@gmail.com>
2015-11-19 23:09:58 -0500

    finished lost.php

--- lost.php
+++ lost.php
@@ -16,8 +16,8 @@ require('/includes/connect_db.php');
 <div id="mainForm">
 <h1>Lost Item Listing</h1>
 <p>Help us find your lost item by giving us a description of it!</p>
-<form>
-   <p> <input type="text" name="listing-name"></p>
+<form action = "lost-1.php">
+   <p>Listing Name: <input type="text" name="listing-name" placeholder="Listing Name"></p>
    <p>Item Type: <select>
         <option value="electronics">Electronic</option>
         <option value="clothing">Clothing</option>
@@ -25,10 +25,14 @@ require('/includes/connect_db.php');
         <option value="book">Book</option>
         <option value="other">Other</option>
     </select></p>
-    <p>Item Color: <input type="text" name="item-color"></p>
-    <p>Location where lost (if known): <select size="7">
+    <p>Item Color: <input type="text" name="item-color" placeholder="Color"></p>
+    <p>Location where lost (if known): </br>
+    <select size="7">
         <?php dropdown_locations($dbc); ?>
         </select></p>
+    <p>Date lost (if known): <input type="date">
+    <p>E-Mail Address: <input type="text" name="email" placeholder="E-Mail Address"> </p>
+    </p><button type="submit">Submit</button>
 </form>
 </div>
 </body>
