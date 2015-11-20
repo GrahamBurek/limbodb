3c21c68b365419cecb475decab2f56b3fdd34492
Maxim Vitkin <max.vitkin@gmail.com>
2015-11-19 21:13:32 -0500

    began work on first lost page

---
+++ lost.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Limbo Lost & Found</title>
+    <!-- Always include this link to the shared stylesheet. To add more style for a specific page or group of pages, add new link element under shared link! -->
+    <link rel="stylesheet" type="text/css" href="/templates/sharedStyle.css">
+</head>
+<body>
+<!-- Navbar include statement: -->
+<?php
+require('/templates/navbar.php');
+?>
+<!-- Main white form for pages: -->
+<div id="mainForm">
+<h1>Lost Item Listing</h1>
+<p>Help us find your lost item by giving us a description of it!</p>
+<form>
+   <p> <input type="text" name="listing-name"></p>
+   <p>Item Type: <select placeholder="Type">
+        <option value="electronics">Electronic</option>
+        <option value="clothing">Clothing</option>
+        <option value="accessories">Accessories</option>
+        <option value="book">Book</option>
+        <option value="other">Other</option>
+    </select></p>
+    <p>Item Color: <input type="text" name="item-color"></p>
+    <p>Location where lost (if known): <select>
+
+        </select></p>
+</form>
+</div>
+</body>
+</html>
