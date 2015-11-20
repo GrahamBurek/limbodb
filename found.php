b3838f3e57dec1d5fcd4925e49dcf27bcf11449c
Maxim Vitkin <max.vitkin@gmail.com>
2015-11-19 23:46:42 -0500

    built found.php, small qol edits to lost.php

---
+++ found.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Limbo Lost & Found</title>
+    <!-- Always include this link to the shared stylesheet. To add more style for a specific page or group of pages, add new link element under shared link! -->
+    <link rel="stylesheet" type="text/css" href="/templates/sharedStyle.css">
+</head>
+<body>
+<!-- Navbar include statement: -->
+<?php
+require('/templates/navbar.php');
+require('/includes/helpers.php');
+require('/includes/connect_db.php');
+?>
+<!-- Main white form for pages: -->
+<div id="mainForm">
+    <!-- Header and description -->
+    <h1>Found something?</h1>
+    <h3>Help us find an owner for your found item by giving us a brief description of it!</h3>
+    <!-- start form -->
+    <form action = "found-1.php">
+        <!--text field for listing name-->
+        <p>Listing Name: <input type="text" name="listing-name" placeholder="Listing Name"></p>
+        <!--drop down with item types -->
+        <p>Item Type: <select>
+                <option value="electronics">Electronic</option>
+                <option value="clothing">Clothing</option>
+                <option value="accessories">Accessories</option>
+                <option value="book">Book</option>
+                <option value="other">Other</option>
+            </select></p>
+        <!-- text field for color-->
+        <p>Item Color: <input type="text" name="item-color" placeholder="Color"></p>
+        <p>Location where found: </br>
+            <!--generates drop down of locations from database-->
+            <select size="7">
+                <?php dropdown_locations($dbc); ?>
+            </select></p>
+        <!-- date field for when item was lost -->
+        <p>Date found: <input type="date">
+            <!-- text field for email address-->
+        <p>E-Mail Address: <input type="text" name="email" placeholder="E-Mail Address"> </p>
+        <!-- submit button-->
+        </p><button type="submit">Submit</button>
+    </form>
+</div>
+</body>
+</html>
