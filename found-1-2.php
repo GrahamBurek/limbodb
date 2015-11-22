d37a62409487aad56633308bf2b55f6ad90d6488
Maxim Vitkin <max.vitkin@gmail.com>
2015-11-22 14:50:04 -0500

    added upload image function

---
+++ found-1-2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Limbo Lost & Found</title>
+    <!-- Always include this link to the shared stylesheet. To add more style for a specific page or group of pages, add new link element under shared link! -->
+    <link rel="stylesheet" type="text/css" href="templates/sharedStyle.css">
+</head>
+<body>
+<!-- Navbar and database include statements: -->
+<?php
+require('/templates/navbar.php');
+require('/includes/helpers.php');
+require('/includes/connect_db.php');
+?>
+<!-- Main white form for pages: -->
+<div id="mainForm">
+    <!-- Header and description -->
+    <h1>Found something?</h1>
+    <h3>Help us find an owner for your found item by giving us a brief description of it!</h3>
+    <!-- start form -->
+    <form action = "found-1.php" method="post" enctype="multipart/form-data">
+        <!--text field for listing name-->
+        <p>Listing Name: <input type="text" name="listing-name" placeholder="Listing Name"></p>
+        <!--drop down with item types -->
+        <p>Item Type: <select name="item-type">
+                <option value="electronics">Electronic</option>
+                <option value="clothing">Clothing</option>
+                <option value="accessories">Accessories</option>
+                <option value="book">Book</option>
+                <option value="other">Other</option>
+            </select></p>
+        <!-- text field for color-->
+        <p>Item Color: <input type="text" name="item-color" placeholder="Color"></p>
+        <p>Location where found: </br>
+            <!--generates drop down of locations from database-->
+            <select size="7" name="location">
+                <?php dropdown_locations($dbc); ?>
+            </select></p>
+        <!-- date field for when item was lost -->
+        <p>Date found: <input type="date">
+            <!-- text field for email address-->
+        <p>E-Mail Address: <input type="text" name="email" placeholder="E-Mail Address">
+        <?php image_upload()?></p>
+        <p><input type="text" name="further-description" placeholder="Further Description" ></p>
+
+        <!-- submit button-->
+        </p><button type="submit" name="submit">Submit</button>
+    </form>
+    <input type="button" onclick="location.href='index.php';" value="Back to Home" />
+</div>
+</body>
+</html>
--- includes/helpers.php
+++ includes/helpers.php
@@ -153,6 +153,40 @@ function check_results($results) {
     echo '<p>SQL ERROR = ' . mysqli_error( $dbc ) . '</p>'  ;
 }
 
+function image_upload(){
+
+if(isset($_REQUEST['submit']))
+{
+    $filename=  $_FILES["imgfile"]["name"];
+    if ((($_FILES["imgfile"]["type"] == "image/gif")|| ($_FILES["imgfile"]["type"] == "image/jpeg") || ($_FILES["imgfile"]["type"] == "image/png")  || ($_FILES["imgfile"]["type"] == "image/pjpeg")) && ($_FILES["imgfile"]["size"] < 200000))
+    {
+        if(file_exists($_FILES["imgfile"]["name"]))
+        {
+            echo "File name exists.";
+        }
+        else
+        {
+            move_uploaded_file($_FILES["imgfile"]["tmp_name"],"uploads/$filename");
+            echo "Upload Successful . <a href='uploads/$filename'>Click here</a> to view the uploaded image";
+        }
+    }
+    else
+    {
+        echo "invalid file.";
+    }
+}
+else
+{
+
+    echo '
+        Upload an Image:<input type="file" name="imgfile"><br>
+    ';
+
+}
+
+}
+
+
 # auto-populate code
 /*function connect_db(){
 
