eb33ae0356ab8f6444b6a12ed7e09bf4853537c8
Maxim Vitkin <max.vitkin@gmail.com>
2015-12-03 00:17:21 -0500

    started manage_listings page

--- manage_listings.php
+++ manage_listings.php
@@ -1,7 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Maxim
- * Date: 12/2/2015
- * Time: 4:38 PM
- */
+# Check if user has logged in to admin interface before generating page:
+session_start();
+if($_SESSION['logged_in'] == true){
+$pid = $_SESSION['pid'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Logon Page</title>
+    <!-- Always include this link to the shared stylesheet. To add more style for a specific page or group of pages, add new link element under shared link! -->
+    <link rel="stylesheet" type="text/css" href="templates/sharedStyle.css">
+</head>
+<body>
+<!-- Navbar include statement: -->
+<?php
+require('includes/connect_db.php');
+require('includes/admin_tools.php');
+?>
+<div id="admin-navbar">
+    <ul>
+        <a href="admin.php"><li>Administrator Panel</li></a>
+        <a href="manage_users.php"><li>Manage Users</li></a>
+        <a href="manage_listings.php"><li>Manage Listings</li></a>
+        <a href="logout.php" style="float:right;"><li>Logout</li></a>
+
+    </ul>
+</div>
+<!-- Main white form for pages: -->
+<div id="mainForm">
+
+</div>
+
+</body>
+    </html>
+    <?php
+
+} else {
+    # Redirect to login since user is not logged in:
+    header('Location: admin_login.php');
+    die("Unauthorized User!");
+}
+?>
