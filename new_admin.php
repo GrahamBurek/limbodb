6784e55c6f229abca840be4a7211d76785fea8e0
Maxim Vitkin <max.vitkin@gmail.com>
2015-12-11 12:06:33 -0500

    adds styling and required asterisks to new_admin.php; adds disabled button styling to manage_users

--- new_admin.php
+++ new_admin.php
@@ -8,7 +8,7 @@ $pid = $_SESSION['pid'];
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Admin Logon Page</title>
+	<title>Create New Admin</title>
 	<!-- Always include this link to the shared stylesheet. To add more style for a specific page or group of pages, add new link element under shared link! -->
 	<link rel="stylesheet" type="text/css" href="templates/sharedStyle.css">
 </head>
@@ -35,17 +35,17 @@ require('includes/admin_tools.php');
 <div id="mainForm">
 
 <!-- User input form to create a new admin -->
-<form action="new_admin.php" method="POST">
+<form action="new_admin.php" method="POST" class="new_admin_form">
 
 	<p><?php make_new_admin($dbc);?></p>
-	<p><input type="text" name="username" placeholder="Username" /></p>
-	<p><input type="text" name="first_name" placeholder="First Name" /></p>
-	<p><input type="text" name="last_name" placeholder="Last Name" /></p>
-	<p><input type="text" name="email" placeholder="E-mail" /></p>
-	<p><input type="password" name="password" placeholder="Password" /></p>
-	<p><input type="password" name="password-repeat" placeholder="Repeat Password"/></p>
-
-	<p><input type="submit" name="new_admin_submit" /></p>
+	<p><span class="required">*</span> = Required</p>
+	<p><input type="text" name="username" placeholder="Username" /><span class="required">*</span></p>
+	<p><input type="text" name="first_name" placeholder="First Name" /><span class="required">*</span></p>
+	<p><input type="text" name="last_name" placeholder="Last Name" /><span class="required">*</span></p>
+	<p><input type="text" name="email" placeholder="E-mail" /><span class="required">*</span></p>
+	<p><input type="password" name="password" placeholder="Password" /><span class="required">*</span></p>
+	<p><input type="password" name="password-repeat" placeholder="Repeat Password"/><span class="required">*</span>
+	<p><input type="submit" name="new_admin_submit" style="padding:10px 20px;"/></p>
 
 </form>
 </div>
