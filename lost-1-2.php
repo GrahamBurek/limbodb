89d966c9d942419ff39ca41aeb4894507881b2e7
liamrharwood <liamrharwood@gmail.com>
2015-12-10 21:37:29 -0500

    added validation for lost-1-2 (let me know if there's bugs)

--- lost-1-2.php
+++ lost-1-2.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,41 +9,56 @@
     <link rel="stylesheet" type="text/css" href="templates/sharedStyle.css">
 </head>
 <body>
-<!-- Navbar and database include statements: -->
-<?php
-require('/includes/helpers.php');
-require('/includes/init.php');
+    <!-- Navbar and database include statements: -->
+    <?php
+    require('/includes/helpers.php');
+    require('/includes/init.php');
 
 # Set sticky variables to the empty string initially:
-$type = "";
-$color = "";
-$location = "";
-$date = "";
+    $item = "";
+    $type = "";
+    $color = "";
+    $location = "";
+    $date = "";
+    $descr = "";
+    $email = "";
+    $status = "";
 
-if (isset($_GET['type'])) {
-    $type = $_GET['type'];
-}
+    if (isset($_GET['type'])) {
+        $type = $_GET['type'];
+    }
 
-if (isset($_GET['color'])) {
-    $color = $_GET['color'];
-}
+    if (isset($_GET['color'])) {
+        $color = $_GET['color'];
+    }
 
-if (isset($_GET['location'])) {
-    $location = $_GET['location'];
-}
+    if (isset($_GET['location'])) {
+        $location = $_GET['location'];
+    }
 
-if (isset($_GET['date'])) {
-    $date = $_GET['date'];
-}
+    if (isset($_GET['date'])) {
+        $date = $_GET['date'];
+    }
 
-?>
-<div id="navbar">
-    <ul>
-        <a href="index.php"><li>LIMBO Lost & Found
-        </li></a><a href="index.php"><li>Home
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $item = $_POST['listing-name'];
+        $location = $_POST['location'];
+        $type = $_POST['item-type'];
+        $color = $_POST['item-color'];
+        $descr = $_POST['further-description'];
+        $date = $_POST['date'];
+        $email = $_POST['email'];
+        $status = $_POST['status'];
+    }
+
+    ?>
+    <div id="navbar">
+        <ul>
+            <a href="index.php"><li>LIMBO Lost & Found
+            </li></a><a href="index.php"><li>Home
         </li></a><a href="found.php"><li>Found Something?
-        </li></a><a href="lost.php"><li class="current">Lost Something?</li></a>
-    </ul>
+    </li></a><a href="lost.php"><li class="current">Lost Something?</li></a>
+</ul>
 </div>
 <!-- Main white form for pages: -->
 <div id="mainForm">
@@ -53,25 +71,68 @@ if (isset($_GET['date'])) {
     <br>
 
     <!-- start form -->
-    <form action="results.php" method="post" enctype="multipart/form-data" class="itemform">
+    <form action="lost-1-2.php" method="post" enctype="multipart/form-data" class="itemform">
+        <?php
+
+        if($_SERVER['REQUEST_METHOD']=="POST"){
+            if(validate_listing($item, $location, $type, $color, $date, $email)){
+
+                // Image uploading and listing creation logic
+
+                $filename =  $_FILES["imgfile"]["name"];
+                $image = "uploads/$filename";
+
+                if (($_FILES["imgfile"]["type"] == "image/gif")|| ($_FILES["imgfile"]["type"] == "image/jpeg") || ($_FILES["imgfile"]["type"] == "image/png")  || ($_FILES["imgfile"]["type"] == "image/pjpeg"))
+                {
+                    if(file_exists($_FILES["imgfile"]["name"]))
+                    {
+                        echo "Image file name exists.";
+
+                    }
+                    else
+                    {
+                        echo "blah";
+                        move_uploaded_file($_FILES["imgfile"]["tmp_name"], "uploads/$filename");
+                        echo "Image upload Successful . <a href='uploads/$filename'>Click here</a> to view the uploaded image";
+
+                        insert_item($dbc, $item, $location, $category, $color, $descr, $date, $email, $status, $image);
+                        $_SESSION['inserted'] = true;
+                        header('Location: index.php');
+                        die("Unauthorized User!");
+                    }
+                }
+                else
+                {
+                    echo "invalid file.";
+                }
+                
+
+                
+
+            } 
+
+        }
+
+
+        ?>
 
         <!--text field for listing name-->
         <p><input type="text" name="listing-name" placeholder="Listing Name"><span class="required">*</span></p>
 
         <!--drop down with item types -->
         <p><select name="item-type">
-                <option value="" disabled selected>Item Type</option>
-                <option value="Electronics" <?php echo ($type == 'Electronics') ? "selected" : "";  ?> >Electronics</option>
-                <option value="Clothing" <?php echo ($type == 'Clothing') ? "selected" : "";  ?> >Clothing</option>
-                <option value="School Supplies" <?php echo ($type == 'School Supplies') ? "selected" : "";  ?> >School Supplies</option>
-                <option value="Other" <?php echo ($type == 'Other') ? "selected" : "";  ?> >Other</option>
-            </select>
+            <option value="" disabled selected>Item Type</option>
+            <option value="Electronics" <?php echo ($type == 'Electronics') ? "selected" : "";  ?> >Electronics</option>
+            <option value="Clothing" <?php echo ($type == 'Clothing') ? "selected" : "";  ?> >Clothing</option>
+            <option value="School Supplies" <?php echo ($type == 'School Supplies') ? "selected" : "";  ?> >School Supplies</option>
+            <option value="Other" <?php echo ($type == 'Other') ? "selected" : "";  ?> >Other</option>
+        </select>
 
-            <span class="required" style="margin-right:40px;">*</span>
+        <span class="required" style="margin-right:40px;">*</span>
 
         <!-- text field for color-->
         <input type="text" name="item-color" placeholder="Color"  <?php echo (!empty($color)) ? 'value="' . $color . '"' : ""; ?> >
-            <span class="required">*</span></p>
+        <span class="required">*</span></p>
         <p></br>
 
 
@@ -84,21 +145,21 @@ if (isset($_GET['date'])) {
             </select></p>
 
 
-        <!-- date field for when item was lost -->
-        <p>Date lost: <input name="date" type="date" value="<?php echo $date; ?>">
+            <!-- date field for when item was lost -->
+            <p>Date lost: <input name="date" type="date" value="<?php echo $date; ?>">
 
-            <!-- text field for email address-->
-        <p><input type="text" name="email" placeholder="E-Mail Address">
+                <!-- text field for email address-->
+                <p><input type="text" name="email" placeholder="E-Mail Address">
 
-            <span class="required" style= "margin-right:45px;">*</span>
-            Upload an Image:<input type="file" name="imgfile"><br></p>
-        <p><textarea name="further-description" placeholder="Further Description"></textarea></p>
-        <input action="action" type="button" class="back-button" value="Back" onclick="history.go(-1);" style="width:75px;"/>
-        <input type="text" name="status" value="Lost" hidden>
+                    <span class="required" style= "margin-right:45px;">*</span>
+                    Upload an Image:<input type="file" name="imgfile"><br></p>
+                    <p><textarea name="further-description" placeholder="Further Description"></textarea></p>
+                    <input action="action" type="button" class="back-button" value="Back" onclick="history.go(-1);" style="width:75px;"/>
+                    <input type="text" name="status" value="Lost" hidden>
 
-        <!-- submit button-->
-        <button type="submit" name="submit" onclick="return confirm('Are you sure all the information is correct?')">Submit</button>
-    </form>
-</div>
-</body>
-</html>
+                    <!-- submit button-->
+                    <button type="submit" name="submit" onclick="return confirm('Are you sure all the information is correct?')">Submit</button>
+                </form>
+            </div>
+        </body>
+        </html>
